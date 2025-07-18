4f200e2eb3f58e1839fb23b153ab821c5ad793f1
root <r.majura@alustudent.com>
2025-07-18 00:11:31 -0700

    poiu

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,6 +2,7 @@
 """ 1-top_ten.py """
 
 import requests
+import sys
 
 def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
@@ -14,3 +15,9 @@ def top_ten(subreddit):
     posts = response.json()['data']['children']
     for post in posts:
         print(post['data']['title'])
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
